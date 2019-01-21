52851ec95521ff46b506ee43928921e5ffb7108c
Nam D. Nguyen <nnguyen83@gmail.com>
2019-01-20 18:42:05 -0600

    Recreate plot 2 and save PNG

---
+++ plot2.R
@@ -0,0 +1,40 @@
+#! /usr/bin/env Rscript
+
+## Exploratory Data Analysis Course Project 1: Creating Plot 2
+
+## Load libraries
+library(readr)
+library(dplyr)
+
+## Set working directory and file names
+setwd("./")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file <- file.path("data", "household_power_consumption.zip")
+
+## Create data directory
+if(!file.exists("data")) {
+  dir.create("data")
+}
+
+## Retrieve data file
+if(!file.exists(file)) {
+  download.file(fileUrl, destfile = file, method = "curl")
+  dateDownloaded <- date()
+  dateDownloaded
+}
+
+## Load data
+dat <- read_delim(file, delim = ";", na = "?",
+                  col_types = cols(Date = col_date(format = "%d/%m/%Y")))
+
+## Filter for analysis dates
+df <- dat %>%
+  filter(Date == "2007-02-01" | Date == "2007-02-02") %>%
+  mutate(datetime = as.POSIXct(paste(Date, Time), format = "%Y-%m-%d %H:%M:%S"))
+
+## Create and save plot 1 as PNG file
+png(filename = "plot2.png")
+plot(df$datetime, df$Global_active_power, type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
